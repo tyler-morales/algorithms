04ccd950aa6a1e90915ac263107672e79ad53017
tyler-morales <tyler.morales.dev@gmail.com>
2021-11-26 16:59:28 -0600

    ✏️  Reformat text

--- Leetcode/001-Two-Sum.js
+++ Leetcode/001-Two-Sum.js
@@ -1,7 +1,6 @@
 /**
 NOTE: 001-Two-Sum
 LINK: https://leetcode.com/problems/two-sum/
-// LINK ./README.md
 
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
@@ -19,18 +18,16 @@ Output: [0,1]
 **/
 
 const twoSum = (nums, target) => {
-    let result = []
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target && i != j)
-                result.push([i, j])
-        }
-
+  let result = []
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target && i != j) result.push([i, j])
     }
-    return result[0]
+  }
+  return result[0]
 }
 
 //
-console.log(twoSum([2, 7, 11, 15], 9));
-console.log(twoSum([3, 3], 6));
-console.log(twoSum([2, 5, 5, 11], 10));
+console.log(twoSum([2, 7, 11, 15], 9))
+console.log(twoSum([3, 3], 6))
+console.log(twoSum([2, 5, 5, 11], 10))
